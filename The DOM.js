79c7acbdd6d53d1d3ab9ae4fe61cb2642462d8f2
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2019-12-09 11:02:39 +0530

    The DOM (Lesson #44 and Lesson #45)

---
+++ The DOM.js
@@ -0,0 +1,4 @@
+// DOM(Document object model) - Created by the browser
+// Def. : When the browser parses an HTML document. It creates a representation in memory of all the elements on the page. This representation is called 'DOM'. It's a tree structure where each element represent by a 'node'.
+console.log(document.location);
+console.log(document.URL);
