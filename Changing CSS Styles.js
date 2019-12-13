f46ea1edbd3fb74e77f2b7e808af5dace882ec93
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2019-12-13 08:23:56 +0530

    Changing CSS Styles (Lesson #50)

---
+++ Changing CSS Styles.js
@@ -0,0 +1,13 @@
+const title = document.querySelector('h1');
+
+// title.setAttribute('style', 'margin: 50px;');
+
+console.log(title.style);
+
+title.style.margin = '50px';
+
+title.style.color = 'crimson';
+
+title.style.fontSize = '5rem';
+
+title.style.margin = ''; // remove or delete margin
