b0860f9b24bf160c14a2ae40fc5942661474e5ef
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2019-12-14 10:28:21 +0530

    Parents Children and Siblings (Lesson #52)

--- Parents Children and Siblings.js
+++ Parents Children and Siblings.js
@@ -9,8 +9,6 @@ Array.from(article.children).forEach(child => {
   child.classList.add('article-element');
 });
 
-
-
 const title = document.querySelector('h2');
 console.log(title.parentElement);
 console.log(title.parentElement.parentElement);
