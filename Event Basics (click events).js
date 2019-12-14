28ce31b74f4811a9ddd7d3f7e8ba0f69c5c38305
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2019-12-14 10:05:11 +0530

    Event Basics (click events) (Lesson #53)

---
+++ Event Basics (click events).js
@@ -0,0 +1,15 @@
+const button = document.querySelector('.button');
+// button.addEventListener('click', () => {
+//   console.log('You just clicked me');
+// });
+
+const items = document.querySelectorAll('li');
+// console.log(items);
+items.forEach(item => {
+  item.addEventListener('click', (e) => {
+    // console.log('item clicked');
+    // console.log(item);
+    // console.log(e.target);
+    e.target.style.textDecoration = 'line-through';
+  });
+});
